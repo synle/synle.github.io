4c354e67d0592ceb92e7200b71b05e5cd7f41b15
Sy Le <lenguyensy@gmail.com>
2021-07-22 13:56:20 -0700

    Update navs.js

--- link/assets/navs.js
+++ link/assets/navs.js
@@ -177,7 +177,13 @@ window.onTestNav = () => {
   const base64URL = document.querySelector("#output").value;
   var win = window.open();
   win.document.write(
-    '<iframe src="' +
+    `
+    <style>
+      body{
+        margin: 0;
+      }
+    </style>
+    `.trim() + '<iframe src="' +
       base64URL +
       '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
   );
