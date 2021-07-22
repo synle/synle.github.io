9c86ed6e7d8372194fd34a6d688ee9c7dd1178e7
Sy Le <lenguyensy@gmail.com>
2021-07-21 19:05:24 -0700

    Update navs.js

--- link/assets/navs.js
+++ link/assets/navs.js
@@ -26,7 +26,7 @@ window.onViewSchema = () => {
 
   const rawSchemaDataDom = `
     <div><h1>Navigation Form</h1></div>
-    <div style="display: flex; margin-bottom: 10px; align-items:center">
+    <div style="display: flex; margin-bottom: 1rem; align-items:center">
       <button onclick="window.onViewLinks(window.getLinkDom(document.querySelector('#input').value))">View Links UI</button>
       <a target="_blank" style="text-align: center; margin: auto;" href="https://github.com/synle/synle.github.io/blob/master/link/assets/navs.js">View Source</a>
     </div>
@@ -52,7 +52,7 @@ window.onViewSchema = () => {
 window.onViewLinks = (linkDomHTML) => {
   // append the extra stuffs
   document.body.innerHTML = linkDomHTML 
-    + `<div style="display: flex; margin-bottom: 10px; align-items:center">
+    + `<div style="display: flex; margin-top: 1rem; align-items:center">
         <button onClick='window.onViewSchema()'>View Schema Source</button>
       </div>`;
 
