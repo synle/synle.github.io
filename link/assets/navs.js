bf46b9bc50fd550954187d4f7a8487032a1ea916
Sy Le <lenguyensy@gmail.com>
2021-07-27 21:26:42 -0700

    Update navs.js

--- link/assets/navs.js
+++ link/assets/navs.js
@@ -42,7 +42,7 @@ window.onViewSchema = () => {
         <button onclick="window.onTestNav()">Test Nav</button>
         <a target="_blank" style="text-align: center; margin: auto;" href="https://github.com/synle/synle.github.io/blob/master/link/assets/navs.js">View Source</a>
       </div>
-      <textarea id='input' placeholder="Bookmarklet Input Schema" wrap="soft" onfocus="window.zoominInput(this)" onblur="window.zoominOutput(this)" onkeyup="window.onGetGeneratedBookmarkletLink(document.querySelector('#input').value)">${output}</textarea>
+      <textarea id='input' placeholder="Bookmarklet Input Schema" wrap="soft" onfocus="window.zoominInput(this)" onblur="window.zoominOutput(this); window.onGetGeneratedBookmarkletLink(document.querySelector('#input').value)">${output}</textarea>
       <textarea id='output' placeholder="Bookmarklet Output" wrap="soft" onfocus="window.zoominInput(this)" onblur="window.zoominOutput(this)"></textarea>
     </div>
   `;
