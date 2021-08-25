b402002b2cecdd9f5510b2838d90ee26608a2add
Sy Le <lenguyensy@gmail.com>
2021-08-25 16:59:50 -0700

    Update navs.js

--- link/assets/navs.js
+++ link/assets/navs.js
@@ -617,6 +617,6 @@ window.prompt = (promptText, promptInput, autoDismiss) => {
     document.title = document.querySelector(".title").innerText.trim();
     
     // set the page fav icon
-    let icon = '📑' && document.head.insertAdjacentHTML('beforeend', `<link rel="icon" href="data:image/svg+xml,${encodeURIComponent(`<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'><text x='0' y='14'>🦄</text></svg>`)}" />`);
+    let icon = '📑'; document.head.insertAdjacentHTML('beforeend', `<link rel="icon" href="data:image/svg+xml,${encodeURIComponent(`<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'><text x='0' y='14'>${icon}</text></svg>`)}" />`);
   });
 })();
