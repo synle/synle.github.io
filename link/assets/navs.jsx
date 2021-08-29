afffce1bee77f1796a1794e8cb186fe0d7c5da06
Sy Le <lenguyensy@gmail.com>
2021-08-29 08:01:15 -0700

    wip

--- link/assets/navs.jsx
+++ link/assets/navs.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect, useRef } from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
 
 (async () => {
   // code start here
