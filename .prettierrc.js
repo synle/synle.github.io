f78ee220967fc017a5075a0fca79c74d01619938
Sy Le <lenguyensy@gmail.com>
2021-08-28 20:56:27 -0700

    fixed format script

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+
+// @trivago/prettier-plugin-sort-imports
+module.exports = {
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all'
+};
+
