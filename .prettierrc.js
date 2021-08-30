79700fe5aa6d529db2c44a9ed68b3ab80abf7465
Sy Le <lenguyensy@gmail.com>
2021-08-29 19:55:54 -0700

    added link app vanilla js
    utils scripts
    
    code format
    
    rearrange paths
    
    rearrange more paths
    
    update package json
    
    clean up package json
    
    clean up the less on the main page
    
    clean up css
    
    clean up the color map
    
    clean up less
    
    code format

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
--- format.sh
+++ format.sh
@@ -1,1 +1,2 @@
-npm run format
+echo '>> format'
+npx prettier --write index.html index.less **/**/**/*.js **/**/**/*.less **/**/*.html
