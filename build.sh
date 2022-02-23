2dec41c85e4ef28343f9ff1635abf6e1d0af9c7c
Sy Le <lenguyensy@gmail.com>
2022-02-22 18:57:56 -0800

    now uses timestamp for the version of the published service worker

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-sh format.sh
+echo '> build index.html'
+node generate-index-html.js
 
-echo 'build css'
+echo '> build css'
 npx -p less lessc index.less index.css
+
+echo '> format code'
+npm run format
+
+echo '> build syle-resume.pdf'
+node generate-resume.js
