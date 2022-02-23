a20214b854760ae1f419a1ae49bc922bac7bec80
Sy Le <lenguyensy@gmail.com>
2022-02-22 18:22:11 -0800

    updated resume

--- generate-index-html.js
+++ generate-index-html.js
@@ -1,7 +1,18 @@
 const Mustache = require('mustache');
 const fs = require('fs');
 
+
+let yoe;
+try {
+  yoe = new Date().getYear() - (2011 - 1900);
+  yoe += '+';
+} catch (e) {
+  yoe = '8+';
+}
+
 const viewData = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+viewData.yoe = yoe;
+
 const template = fs.readFileSync('index.mustache', 'utf8');
 const output = Mustache.render(template, viewData);
 
