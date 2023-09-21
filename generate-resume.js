3f3d31f54833526989d3c7479a43e940f23913ea
Sy Le <lenguyensy@gmail.com>
2023-09-20 19:32:07 -0700

    minor updates

--- generate-resume.js
+++ generate-resume.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 (async () => {
   const browser = await puppeteer.launch();
 
-  let contentHtml = fs.readFileSync('index.html', 'utf8');
-  let contentCss = fs.readFileSync('index.css', 'utf8');
-  contentHtml += `<style>${contentCss}</style>`;
   const page = await browser.newPage();
-  await page.setContent(contentHtml);
-  await page.pdf({ path: 'syle-resume.pdf', format: 'a4' });
+  await page.setContent(`
+    ${fs.readFileSync('index.html', 'utf8')}
+    <style>${fs.readFileSync('index.css', 'utf8')}</style>
+  `.trim());
+  // await page.pdf({ path: 'syle-resume.pdf', format: 'a4' });
+  await page.pdf({ path: 'syle-resume.pdf', format: 'a4', margin: { top: '5px', bottom: '5px', left: '10px', right: '10px' } });
 
   await browser.close();
 })();
