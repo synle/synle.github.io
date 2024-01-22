a7b65af5fe843815763d9a81dbc64edbe4d7bbab
synle <synle@users.noreply.github.com>
2024-01-22 17:46:21 +0000

    CI Build - Prettier & LESS build & PDF generation

--- src/data-full.js
+++ src/data-full.js
@@ -2,7 +2,7 @@ String.prototype._reformatItems = function () {
   return this.split('\n')
     .map((s) => s.trim())
     .filter((s) => s)
-    .map(s => `${s}.`);
+    .map((s) => `${s}.`);
 };
 
 const resp = {
