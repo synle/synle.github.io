bc0b4e7351cc05e36663c0bf0d2a94f509558dfd
Sy Le <lenguyensy@gmail.com>
2024-01-22 09:35:29 -0800

    reformat the json

--- src/data-short.js
+++ src/data-short.js
@@ -1,7 +1,14 @@
+String.prototype._reformatItems = function () {
+  return this.split('\n')
+    .map((s) => s.trim())
+    .filter((s) => s)
+    .map(s => `${s}.`);
+};
+
 const resp = {
   Summary: `
-    I'm Sy Le, a software engineer with a decade of experience at LinkedIn, Salesforce, and Yahoo. I built software for CRM, marketing automation, e-commerce, and monitoring platforms. My skills include JavaScript/TypeScript and React for front-end, and Node.js / RemixJS / Express for back-end development.
-    `,
+I'm Sy Le, a software engineer with a decade of experience at LinkedIn, Salesforce, and Yahoo. I built software for CRM, marketing automation, e-commerce, and monitoring platforms. My skills include JavaScript/TypeScript and React for front-end, and Node.js / RemixJS / Express for back-end development.
+    `.trim(),
   Experience: {
     rows: [
       {
@@ -10,15 +17,23 @@ const resp = {
         location: 'Sunnyvale, CA',
         from: '05/2017',
         to: 'Present',
-        items: [
-          `As a Software Engineer at LinkedIn Enterprise Learning and Productivity Engineering, I leveraged my expertise in a wide range of technologies, including JavaScript, Ember, React, Node.js, Java, Spring Boot, Python, Flask, MySQL, Redis, and Azure to drive successful projects.`,
-          `I provided expert guidance on UI frameworks and Azure DevOps CI/CD pipelines to drive success. My insights and expertise have helped the team streamline development and achieve outstanding results.`,
-          `Working on <a href='//linkedin.com/help/linkedin'>Linkedin Support Portal</a> that manages customers' chats and offline cases on Microsoft Dynamics365 CRM using C#, .NET Core, React and Microsoft Fluent UI.`,
-          `As a Full-Stack Engineer Team Lead, I led the development of Nucleus, an end-to-end purchase flow automation platform, featuring a modern React-and-Redux Front-End and a robust Spring Boot back-end hosted on Azure, with integrations with ServiceNow and JIRA.`,
-          `As a skilled full-stack developer, I have played a key role in redesigning Sendbloom's codebase, using the latest tools and technologies such as TypeScript, React, Redux, Node.js, Express, Redis, and MySQL. With my contributions, the platform is now more efficient, and user-friendly.`,
-          `I integrated <a href='//linkedin.com/learning/'>Linkedin Learning</a> with Slideshare DocViewer, enabling end-to-end custom document content creation and viewing, which provided instructors with more options to organize and prepare their course outlines.`,
-          `At the LinkedIn GOPs Expo 2017, I developed and presented the Room Booking Assistant (RoomBA), an innovative bot that intelligently identifies the best meeting rooms and books them on behalf of users, simplifying the process and saving valuable time.`,
-        ],
+        items: `
+As a Software Engineer at LinkedIn Enterprise Learning and Productivity Engineering, I leveraged my expertise in a wide range of technologies, including JavaScript, Ember, React, Node.js, Java, Spring Boot, Python, Flask, MySQL, Redis, and Azure to drive successful projects
+
+I provided expert guidance on UI frameworks and Azure DevOps CI/CD pipelines to drive success. My insights and expertise have helped the team streamline development and achieve outstanding results
+
+Working on <a href='//linkedin.com/help/linkedin'>Linkedin Support Portal</a> that manages customers' chats and offline cases on
+
+Microsoft Dynamics365 CRM using C#, .NET Core, React and Microsoft Fluent UI
+
+As a Full-Stack Engineer Team Lead, I led the development of Nucleus, an end-to-end purchase flow automation platform, featuring a modern React-and-Redux Front-End and a robust Spring Boot back-end hosted on Azure, with integrations with ServiceNow and JIRA
+
+As a skilled full-stack developer, I have played a key role in redesigning Sendbloom's codebase, using the latest tools and technologies such as TypeScript, React, Redux, Node.js, Express, Redis, and MySQL. With my contributions, the platform is now more efficient, and user-friendly
+
+I integrated <a href='//linkedin.com/learning/'>Linkedin Learning</a> with Slideshare DocViewer, enabling end-to-end custom document content creation and viewing, which provided instructors with more options to organize and prepare their course outlines
+
+At the LinkedIn GOPs Expo 2017, I developed and presented the Room Booking Assistant (RoomBA), an innovative bot that intelligently identifies the best meeting rooms and books them on behalf of users, simplifying the process and saving valuable time
+          `._reformatItems(),
       },
       {
         title: 'Senior Member of Technical Staff',
@@ -26,12 +41,15 @@ const resp = {
         location: 'San Francisco, CA',
         from: '04/2015',
         to: '05/2017',
-        items: [
-          `Software Engineer for SalesforceIQ, and Aura using JavaScript, Angular, React, Salesforce Aura / Lightning, Node.js, Express, and Java.`,
-          `Contributed significantly to <a href='//store.office.com/en-us/app.aspx?assetid=WA104379334&amp;ui=en-US&amp;rs=en-US&amp;ad=US&amp;appredirect=false'>SalesforceIQ Inbox Desktop</a>, and providing leadership and mentorship to junior team members.`,
-          `Built various prototypes, including an Inbox Desktop Email client with React and Electron and a Desktop Dialer with Twilio.`,
-          `Won the SalesforceIQ Hackday Fall 2016 Most Creative award with the predictive email compose suggestion project and won the SalesforceIQ Hackday Spring 2017 Most Heroic award with the mass email project.`,
-        ],
+        items: `
+Software Engineer for SalesforceIQ, and Aura using JavaScript, Angular, React, Salesforce Aura / Lightning, Node.js, Express, and Java
+
+Contributed significantly to <a href='//store.office.com/en-us/app.aspx?assetid=WA104379334&amp;ui=en-US&amp;rs=en-US&amp;ad=US&amp;appredirect=false'>SalesforceIQ Inbox Desktop</a>, and providing leadership and mentorship to junior team members
+
+Built various prototypes, including an Inbox Desktop Email client with React and Electron and a Desktop Dialer with Twilio
+
+Won the SalesforceIQ Hackday Fall 2016 Most Creative award with the predictive email compose suggestion project and won the SalesforceIQ Hackday Spring 2017 Most Heroic award with the mass email project
+          `._reformatItems(),
       },
       {
         title: 'Tech Yahoo, Software Dev Engineer',
@@ -39,9 +57,9 @@ const resp = {
         location: 'Sunnyvale, CA',
         from: '07/2014',
         to: '04/2015',
-        items: [
-          `As a Front-End engineer on the Yahoo Monitoring team, I worked on the redesign of the YAMAS 2.0 Dashboard using JavaScript, Angular, Node.js, and Express. Our redesign won an award at the internal Yahoo TechPulse 2014 conference. Additionally, I set up a CI and CD pipeline for Express back-end applications using Jenkins CI and Chef CD to streamline building and deployment.`,
-        ],
+        items: `
+As a Front-End engineer on the Yahoo Monitoring team, I worked on the redesign of the YAMAS 2.0 Dashboard using JavaScript, Angular, Node.js, and Express. Our redesign won an award at the internal Yahoo TechPulse 2014 conference. Additionally, I set up a CI and CD pipeline for Express back-end applications using Jenkins CI and Chef CD to streamline building and deployment
+          `._reformatItems(),
       },
       {
         title: 'Software Engineer',
@@ -49,9 +67,9 @@ const resp = {
         location: 'San Jose, CA',
         from: '08/2013',
         to: '07/2014',
-        items: [
-          `As a Software Engineer on eBay Transaction Flow and Registration team, I implemented the responsive Guest Checkout Registration and integrated Selenium UI automation to extend UI integration test coverage to iPad and iPhone with JavaScript, Java and Spring MVC Framework.`,
-        ],
+        items: `
+As a Software Engineer on eBay Transaction Flow and Registration team, I implemented the responsive Guest Checkout Registration and integrated Selenium UI automation to extend UI integration test coverage to iPad and iPhone with JavaScript, Java and Spring MVC Framework
+          `._reformatItems(),
       },
       {
         title: 'Software Engineer',
@@ -59,9 +77,9 @@ const resp = {
         location: 'San Jose, CA',
         from: '10/2011',
         to: '08/2013',
-        items: [
-          `As a Full-Stack Engineer, I used JavaScript, PHP, Node.js, C#, MySQL, Cassandra, and RabbitMQ to build key components of the <a href='//www.etrigue.com/'>eTrigue Demand Center</a>. This includes Prospect List, Campaign Builder, ETL Jobs, and vendor integration with Cisco Webex and Citrix GoToWebinar.`,
-        ],
+        items: `
+As a Full-Stack Engineer, I used JavaScript, PHP, Node.js, C#, MySQL, Cassandra, and RabbitMQ to build key components of the <a href='//www.etrigue.com/'>eTrigue Demand Center</a>. This includes Prospect List, Campaign Builder, ETL Jobs, and vendor integration with Cisco Webex and Citrix GoToWebinar
+        `._reformatItems(),
       },
       {
         title: 'Software Engineer',
@@ -69,9 +87,9 @@ const resp = {
         location: 'San Jose, CA',
         from: '08/2010',
         to: '10/2011',
-        items: [
-          `As a Full-Stack Engineer, I built Clinic Tree, a clinic portal that includes features like patient record charting, appointment management, and a content management system. The portal was built using JavaScript, C#, and .NET.`,
-        ],
+        items: `
+As a Full-Stack Engineer, I built Clinic Tree, a clinic portal that includes features like patient record charting, appointment management, and a content management system. The portal was built using JavaScript, C#, and .NET
+        `._reformatItems(),
       },
     ],
   },
