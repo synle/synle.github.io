8d70c4ff4717e093dc4d5b312e5403d898142540
synle <synle@users.noreply.github.com>
2024-01-23 20:59:30 +0000

    CI Build - Prettier & LESS build & PDF generation

--- src/data.common.js
+++ src/data.common.js
@@ -2,5 +2,5 @@ String.prototype._reformatItems = function () {
   return this.split('\n')
     .map((s) => s.trim())
     .filter((s) => s)
-      .map((s) => `${s.replace(/\.*\.$/, '')}.`);
+    .map((s) => `${s.replace(/\.*\.$/, '')}.`);
 };
