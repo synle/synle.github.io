0a453e844c02a08ff8adb795b4f5f0e661d71b40
synle <synle@users.noreply.github.com>
2024-02-08 18:51:11 +0000

    CI Build - Prettier & LESS build & PDF generation

--- generate-index-html.js
+++ generate-index-html.js
@@ -44,9 +44,13 @@ const dataShort = parseConfigFile('src/data-short.js', dataBase);
 // default short
 generateView(dataShort, 'index.html');
 
-for(const file of fs.readdirSync('src')){
-  if(!file.includes('base')&& file.includes('data-') && file.includes('.js')){
-    const inputFile = 'src/' + file
+for (const file of fs.readdirSync('src')) {
+  if (
+    !file.includes('base') &&
+    file.includes('data-') &&
+    file.includes('.js')
+  ) {
+    const inputFile = 'src/' + file;
     const outputFile = file.replace('.js', '').replace('data-', '') + '.html';
 
     console.log('> ', inputFile, outputFile);
